b14797723908540b6ea548308c491920fa33be10
itimor <itimor@126.com>
2018-01-10 09:07:17 +0800

    create

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" soccer bet main function file """
+
+import spider
+import portfoliomodel
+import time
+
+m_match_ids = spider.crawl_match_list()
+
+for m_match_id in m_match_ids:
+    print(m_match_id)
+    m_match = spider.get_match(m_match_id)
+
+    portfolio = portfoliomodel.best_portfolio(m_match)
+
+    m_match.display()
+    portfolio.display()
+    time.sleep(4)
